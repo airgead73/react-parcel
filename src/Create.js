78ba7c9231a56913fad35b6e8fe1af63c59429ab
Dan Moneypenny <catullus1973@gmail.com>
2021-01-28 05:36:14 -0800

    update

--- src/Create.js
+++ src/Create.js
@@ -3,6 +3,23 @@ const Create = () => {
   return ( 
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label htmlFor="">Blog Title</label>
+        <input 
+          type="text"
+          required
+        />
+        <label htmlFor="">Blog body</label>
+        <textarea 
+          required
+        ></textarea> 
+        <label htmlFor="">Blog author:</label> 
+        <select name="" id="">
+          <option value="mario">mario</option>
+          <option value="luigi">luigi</option>
+        </select>  
+        <button>add blog</button>  
+      </form>
     </div>
    );
 }
