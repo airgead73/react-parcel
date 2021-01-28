493cc2d70c581b24352947e1c748b1a36398b30b
Dan Moneypenny <catullus1973@gmail.com>
2021-01-28 05:49:35 -0800

    remove old files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route path="/create">
-              <Create/>
-            </Route> 
-            <Route path="/blogs/:id">
-              <BlogDetails/>
-            </Route>                        
-          </Switch>
-        </div>
-      </div>
-    </Router>
-
+      <div>Hello </div>
   );
 }
 
